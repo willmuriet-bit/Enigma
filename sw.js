eca143bf6ec42aa02f764634382cbb6d9f3b4865
Stats <will.muriet@gmail.com>
2026-02-24 05:34:39 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,45 @@
 const CACHE_NAME = 'enigma-v1';
-const urlsToCache = [
-  '.',
-  'index.html',
-  'manifest.json'
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './manifest.json',
+    './words.json',
+    './icon-192.png'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+            .then(() => self.skipWaiting())
+    );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.method !== 'GET') return;
+    
+    event.respondWith(
+        caches.match(event.request)
+            .then((cachedResponse) => {
+                if (cachedResponse) return cachedResponse;
+                return fetch(event.request);
+            })
+            .catch(() => {
+                if (event.request.headers.get('accept') && event.request.headers.get('accept').includes('text/html')) {
+                    return caches.match('./index.html');
+                }
+            })
+    );
 });
